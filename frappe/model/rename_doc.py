79498de76d95193874d0a6c056a8aa6cfd48f5ec
Devin Slauenwhite <devin.slauenwhite@gmail.com>
2025-06-25 17:22:37 -0400

    fix: don't attempt to rename virtual child docs

--- frappe/model/rename_doc.py
+++ frappe/model/rename_doc.py
@@ -399,6 +399,8 @@ def rename_doctype(doctype: str, old: str, new: str) -> None:
 def update_child_docs(old: str, new: str, meta: "Meta") -> None:
 	# update "parent"
 	for df in meta.get_table_fields():
+		if frappe.get_meta(df.options).is_virtual:
+			continue
 		(
 			frappe.qb.update(df.options)
 			.set("parent", new)
